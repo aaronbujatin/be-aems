77fdeb42d9d14db944d74b1247072fbafca6faa8
Aaron Bujatin <aaronbujatin@gmail.com>
2023-09-23 18:19:10 +0800

    Add files via upload

--- src/main/java/com/aaronbujatin/beaems/user/UserController.java
+++ src/main/java/com/aaronbujatin/beaems/user/UserController.java
@@ -3,14 +3,12 @@ package com.aaronbujatin.beaems.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users")
+@CrossOrigin("*")
 public class UserController {
 
     private final UserService userService;
